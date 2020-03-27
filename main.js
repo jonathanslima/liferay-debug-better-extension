e5d11746afb4b506229341c6954fee2b9d4f9e1d
Lima <jonathan.da.s.lima@accenture.com>
2020-03-27 14:35:58 -0300

    plugin working 1.0

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-alert('oi')
+document.addEventListener('DOMNodeInserted', function (event) {
+	var url = location.href;
+	var ext = '?js_fast_load=0&css_fast_load=0&strip=0';
+
+	if(url.indexOf(ext) == -1){
+		var arrUrls = [
+			'list of urls here'
+		];
+		
+		arrUrls.map(function(i){
+			if(url.indexOf(i) !== -1 ){
+				if(location.href.slice(-1) == '/'){
+					var newUrl = location.href.slice(0, -1)
+				}else{
+					var newUrl = url
+				}
+				location.href = newUrl + ext;
+			}
+		})
+	}
+});
+
+
+
